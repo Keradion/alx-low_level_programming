6f325404ddde388cff3d4d3d8d380d93ba94065d
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-06 08:02:21 -0700

    A c program that prints all digits starting from 0-9 using a while loop

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,11 @@
 #include <stdio.h>
 
 /**
- * main - A C language program that prints all single digit numbers of base 10
+ * main - Entry point
+ * Description : a C program that prints all single digit numbers of base 10
  * starting from 0 followed by a new line .
  *
- * return - success (0)
+ * Return : Always 0 (Success)
  *
  */
 
