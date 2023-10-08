4a73ab5771a62d4a0fc7a73f5fee6645fc3ec931
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-08 09:27:44 -0700

    A C language Program That Print all Lower Case Alphabetic Charachters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,32 @@
+/*
+ * Author : Daniel Berihun
+ * Title : 2-print_alphabet.c
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description : a C program that prints all alphabetic charachters 
+ * in lowercase followed by a new line .
+ *
+ * Return : Always 0 (Success)
+ *
+ */
+
+int main() {
+    
+int i = 97;
+    
+    while(i < 123 )
+    
+    {
+    putchar(i);
+    i++ ;
+    } 
+ 
+    putchar('\n');
+
+  return 0;
+}
