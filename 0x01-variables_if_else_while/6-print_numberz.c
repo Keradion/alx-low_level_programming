73005302233e0e3017a5a95b4537b5c6a8484a3c
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-08 23:12:19 -0700

    A c program that print ditis from 0 upto 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 /**
- * main - a C program that prints all single digit numbers 
+ * main - a C program that prints all single digit numbers
  * of base 10 starting from 0
  *
  * Return: Always 0
