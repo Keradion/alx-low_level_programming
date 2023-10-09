c5f85414cd165464be35f3fc3c4514d76af42d40
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-08 22:36:08 -0700

    3rd task new line added

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,20 @@
+
+/*
+ * Author : Daniel Berihun
+ * Title : 3-print_alphabets.c
+ *
+ */
+
 #include <stdio.h>
 
+/**
+ * main - a C program that prints all lowercase then upercase alphabetic charach * ters
+ *
+ * return: Always 0
+ */
 
-int main (void) 
+
+int main(void) 
 { 
 {
 int i = 97;
@@ -11,16 +24,13 @@ putchar(i);
 i++;
 } 
 } 
-    
 {
 int j = 65;
-    
 while (j < 91)
 {
 putchar(j);
 j++;
 }
-  
 }
 putchar('\n');
 return (0);
