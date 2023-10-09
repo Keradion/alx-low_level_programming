e46184a8e2934cd3600e31eb117e81d482476112
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-08 22:01:06 -0700

    output correction on the third if statment

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,22 +16,24 @@
 
 int main(void)
 {
-    int n;
+int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    /* your code goes there */
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-    if (n>0)
-        {printf("%d is positive",n);
-        putchar('\n');
-	}
-   else if (n<0)
-        { printf("%d is negative",n);
-        putchar('\n'); 
-	}
-    else
-        { printf("%d is zero'\n' ",n);
-        putchar('\n'); }
-    return (0);
+if (n>0)
+{
+printf("%d is positive",n);
+putchar('\n');
+}
+else if (n<0)
+{
+printf("%d is negative",n);
+putchar('\n'); 
+}
+else
+{ 
+printf("%d is zero",n);
+putchar('\n'); }
+return (0);
 }
