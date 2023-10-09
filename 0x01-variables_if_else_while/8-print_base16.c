5cfbab8d88e118a75713f2bdfc7650757c55f109
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-08 23:25:44 -0700

    A program that print all the hexadecimal numbers in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,35 @@
+/*
+ * Author : Daniel Berihun
+ * Title : 8-print_base16.c
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - a C program that prints all the numbers of hexadecimal in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+{
+int i = 48;
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+}
+{
+int i = 97;
+while (i < 103)
+{
+putchar(i);
+i++;
+}
+}
+putchar('\n');
+return (0);
+}
