e234a3c0d74fa146e6f1f3151420c55cc35cbb96
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-08 21:54:24 -0700

    A c program that check a random number is + - or 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,38 @@
+/*
+ * Author : Daniel Berihun
+ * Title : 2-print_alphabet.c
+ *
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - a C program that check whether a given random number is + - or 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    /* your code goes there */
+
+    if (n>0)
+        {printf("%d is positive",n);
+        putchar('\n');
+	}
+
+}    else if (n<0)
+        { printf("%d is negative",n);
+        putchar('\n'); 
+	}
+    else
+        { printf("%d is zero'\n' ",n);
+        putchar('\n'); }
+    return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main (void) 
+{ 
+{
+int i = 97;
+while (i < 123)
+{
+putchar(i);
+i++;
+} 
+} 
+    
+{
+int j = 65;
+    
+while (j < 91)
+{
+putchar(j);
+j++;
+}
+  
+}
+return (0);
+}
