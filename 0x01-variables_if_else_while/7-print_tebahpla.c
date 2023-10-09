a0e6729747ea8f80bb64535d88890386ba8c55a7
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-08 23:17:54 -0700

    a program that print letters in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+
+/*
+ * Author : Daniel Berihun
+ * Title : 7-print_tebahpla.c
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - a C program that print all lowecase letters in reverse  
+ *
+ * Return: Always 0
+ */
+
+int main(void) 
+{
+int i = 122;
+while (i > 96)
+{
+putchar(i);
+--i;
+} 
+return 0;
+}
