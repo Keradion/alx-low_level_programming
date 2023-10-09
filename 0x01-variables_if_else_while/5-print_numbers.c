a4d89c190b9c4eff47864490442be6e57bd9e3b9
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-08 22:50:46 -0700

    description added for return value of main

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,7 +11,7 @@
  * Description : a C program that prints all single digit numbers of base 10
  * starting from 0 followed by a new line .
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 
 int main(void)
