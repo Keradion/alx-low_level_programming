8e72b4662bc973e819ce2ae741190f80a99b8fba
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-08 22:43:31 -0700

    A program that print lowercase letters except p & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+
+/*
+ * Author : Daniel Berihun
+ * Title : 4-print_alphabet.c
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - A C program that prints all lowercase alphabetic charachters except P & q
+ *
+ * Return: Always 0
+ */
+
+int main() 
+{
+int i = 97;
+while(i < 123)
+{
+if (i != 112 && i != 113)
+putchar(i);
+i++ ;
+} 
+putchar('\n');
+return (0);
+}
