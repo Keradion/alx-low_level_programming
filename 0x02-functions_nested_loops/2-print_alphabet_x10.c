0b79775e0b93752d772e55740ebae6d7046375d9
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-10 01:16:04 -0700

    a function that prints lowercase letters 10X from a-z

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,23 @@
  * Return: Always 0
  */
 
-void print_alphabet();
+void print_alphabet_x10();
 
 int main(void)
 {
-	print_alphabet();
+	print_alphabet_x10();
 
 	return (0);
 
 }
 
-void print_alphabet()
+void print_alphabet_x10()
 {
 	int i = 97;
+	int j = 0;
+
+		while ( j < 10 )
+		{
 	while (i < 123 )
 	{
 		_putchar(i);
@@ -26,6 +30,7 @@ void print_alphabet()
 	}
 
 	_putchar('\n');
-
+	i = 97;	
+	j++;}
 	return;
 }
