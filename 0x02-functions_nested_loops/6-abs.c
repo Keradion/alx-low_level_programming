fbf2b8e565691bd654a090e2e456818d7c74b2ea
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-10 13:46:42 -0700

    description added for _abs()

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 /**
- *_abs() - determine the absolute value of a given number
+ *_abs - determine the absolute value of a given number
  *@n: number
  * Return: Always 0
  */
 
 int _abs(int n)
 {
+
 if (n > 0)
 return (n);
 else if (n == 0)
