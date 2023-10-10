e76e6099d81f6d1219aa02b31a6d11efcc968ca1
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-10 13:34:09 -0700

    a function that determine absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * abs : determine the absolute value of a given number
+ *@n: number
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+if (n < 0 || n > 0)
+return (1);
+else
+return (0);
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * main - check the code for abs function
+ *
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+int r;
+r = _abs(-1);
+printf("%d\n", r);
+return (0);
+}
