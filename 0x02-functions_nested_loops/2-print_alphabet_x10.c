165fd2afffaccbec4141e649ba77a28b1296548b
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-10 04:54:30 -0700

    removing space

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,11 @@
 /**
  * print_alphabet_x10  - a function that prints letters in lowercase_10
  */
+
 void print_alphabet_x10(void)
 {
 int i = 97;	
 int j = 0;
-
 while (j < 10)
 {
 while (i < 123)
