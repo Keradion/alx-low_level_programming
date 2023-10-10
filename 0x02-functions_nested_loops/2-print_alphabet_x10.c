8f53f6cbe901570f5bc73ed533afaffe539397a5
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-10 04:20:20 -0700

    return value removed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@
 
 
 
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	int i = 97;
 	int j = 0;
 
-		while ( j < 10 )
-		{
-	while (i < 123 )
+		while (j < 10)
+
+	while (i < 123)
 	{
 		_putchar(i);
 		i++;
@@ -23,5 +23,6 @@ void print_alphabet_x10()
 
 	_putchar('\n');
 	i = 97;	
-	j++;}
+	j++;
+		}
 }
