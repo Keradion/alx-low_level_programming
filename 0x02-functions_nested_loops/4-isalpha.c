0037ae51d44c975a5f7e68f3c15dd9796c975cec
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-10 17:35:41 -0700

    alphabetic charachter checker function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _isalpha - check for alphabetic charachter
- * @c; ASCII value
+ * @c: ASCII value
  *
  * Return: 1 if alphabetic, 0 if none
  */
 
 int _isalpha(int c)
 {
-if ( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
 else
 return (0);
