7280ec096e093620bd73c213f3485f81cb7077d2
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-11 15:24:46 -0700

    a function that print day hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - print day hours from 00:00 to 23:59
+ *
+ * Return: void
+ */ 
+
+void jack_bauer(void)
+{
+int i = 0;
+int j = 0;
+int k = 0; 
+int l = 0;
+while (i < 3)
+{
+while (j < 10)
+{ 
+while (k < 6)
+{
+while (l < 10)
+{
+if (!( i == 2 && j == 4 && k == 0 && l == 0))
+{ 
+printf("%d", i);
+printf("%d", j);
+printf(":%d", k);
+printf("%d", l);
+printf("\n");
+l++;
+}
+
+}
+k++;
+l = 0;
+}
+j++;
+k = 0;
+l = 0;
+}
+i++;
+k = 0;
+l = 0;
+j = 0;
+}
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * main - check the code for jack_bauer function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+jack_bauer();
+return (0);
+}
