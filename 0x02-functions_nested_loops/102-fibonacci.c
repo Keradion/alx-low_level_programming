da9c30b110091f18dfa3fdc3fd3a0ea108fd0490
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-11 05:11:55 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,21 +1,13 @@
-#include "main.h"
 #include <stdio.h>
 /**
- * fibonacci - a function that prints the first 50 numbers of the series
+ * main - a function that prints the first 50 numbers of the series
  * @first_element: first digit of the series
- * @second_element: second digit of the series
- * @next_element: digit that follows the first and second digits
  *
  * Return: Always 0
  */
 
-int main()
-{
-	fibonacci();
-	return (0);
-}
 
-int fibonacci(void)
+int main()
 {
 int first_element = 1;
 int second_element = 2;
@@ -24,14 +16,12 @@ int i = 1;
 while (i <= 50)
 {
 printf("%d", first_element);
-printf(",");
-printf("%d ", second_element);
+if (i < 50)
+{printf(", ");
+}
 next_element = first_element  + second_element;
 first_element = second_element;
 second_element = next_element;
-if (next_element < 0 )
-next_element = (- next_element);
-printf("%d", next_element);
 i++;
 }
 printf("\n");
