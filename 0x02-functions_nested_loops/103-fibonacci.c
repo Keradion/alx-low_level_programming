9b34fb8e7388c45feb381ac15c4e5abe581dc4a1
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-11 12:15:24 -0700

    a function that return sum of even numbers inside fibonacci series starting from 1-4M

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - a function that prints sum of even numbers in fibonacci starting from 1-4M
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+unsigned long int first_element = 1;
+unsigned long int second_element = 2;
+unsigned long int next_element = 0;
+unsigned long int sum = 0;
+
+while (first_element < 4000000)
+{
+if (first_element % 2 == 0)
+{
+sum = sum + first_element;
+}
+next_element = first_element + second_element;
+first_element = second_element;
+second_element = next_element;
+}
+printf("%lu", sum );
+printf("\n");
+return 0;
+}
