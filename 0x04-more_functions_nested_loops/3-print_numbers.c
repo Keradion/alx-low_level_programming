2b1d8e503068c105f9f0d98992d73c00ad466452
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-12 05:28:41 -0700

    a function that prints numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 3-print_numbers - print numbers from 0-9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{printf("%d", i);
+i++;
+}
+printf("\n");
+}
