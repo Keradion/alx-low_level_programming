081d31807a054a1a70619ee8af44a2f1fa079cd1
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-12 13:41:02 -0700

    betty style and documenation problems resolved

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,36 +4,31 @@
 /**
  * more_numbers - prints numbers from 0 - 14 10x
  *
- * Returns: void
+ * Return: void
  */
 
 void more_numbers(void)
 {
-  
-  int i = 48;
-  int j = 48;
-  int k = 48;
-
- while(k < 58 )
- {
-  while (i < 58)
-  {
-      _putchar(i);
-      i++;
-  }
-  
-  i = 49;
-  
-  while ( j < 53)
-  {
-      _putchar(i);
-      _putchar(j);
-      j++;
-      
-  }
-  printf("\n");
-  i = 48;
-  j = 48;
-  k++;
- }
+int i = 48;
+int j = 48;
+int k = 48;
+while (k < 58)
+{
+while (i < 58)
+{
+_putchar(i);
+i++;
+}
+i = 49;
+while (j < 53)
+{
+_putchar(i);
+_putchar(j);
+j++;
+}
+printf("\n");
+i = 48;
+j = 48;
+k++;
+}
 }
