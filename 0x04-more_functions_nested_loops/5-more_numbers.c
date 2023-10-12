26dc095c8d31c5ea6a314c5e6562899852d1a453
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-12 13:33:04 -0700

    a function that print 0-14 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints numbers from 0 - 14 10x
+ *
+ * Returns: void
+ */
+
+void more_numbers(void)
+{
+  
+  int i = 48;
+  int j = 48;
+  int k = 48;
+
+ while(k < 58 )
+ {
+  while (i < 58)
+  {
+      _putchar(i);
+      i++;
+  }
+  
+  i = 49;
+  
+  while ( j < 53)
+  {
+      _putchar(i);
+      _putchar(j);
+      j++;
+      
+  }
+  printf("\n");
+  i = 48;
+  j = 48;
+  k++;
+ }
+}
