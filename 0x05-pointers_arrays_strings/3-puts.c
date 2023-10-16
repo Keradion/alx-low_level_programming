ada7622e8aa0d82b1a97fe2d2d770f227f86a3e2
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-16 15:34:06 -0700

    betty

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,16 +5,16 @@
  * @str: string  pointer variable
  *
  * Return: void
- */ 
+ */
 
 void _puts(char *str)
 {
 	int i = 0;
 	
-	while (str[i] != '\0')
+	while (str[i] != '0')
 	{
 		_putchar(str[i]);
 		i++;
 	}
-	_putchar('\n');
+	_putchar('n');
 }
