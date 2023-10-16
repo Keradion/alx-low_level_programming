30e25e55c31880f6a85674da093b0248038ee61a
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-16 05:25:44 -0700

    a function taht update value pointed by a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer & update the value it points
+ * @*n: pointer variable
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
