4abc2dd2b20ec6ea3c8133b473547e4dfc8915b5
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-16 15:36:11 -0700

    removing trailing whitespaces

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,15 +5,14 @@
  * @str: string  pointer variable
  *
  * Return: void
- */
-
-void _puts(char *str)
+ */ 
+ void _puts(char *str)
 {
 	int i = 0;
-	
-	while (str[i] != '0')
+
+while (str[i] != '\0')	
 	{
-		_putchar(str[i]);
+	_putchar(str[i]);
 		i++;
 	}
 	_putchar('n');
