93e7025a2c211d0e184335c776f054b176f2c1cb
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-16 19:00:11 -0700

    copy string contents

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy one string into another string 
+ * @src: string pointer variable
+ * @dest: string pointer variable
+ *
+ * Return: char *_strcpy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int len = 0;
+
+	while ( src[i] != '\n')
+	{
+		len++;
+		i++;
+	}
+	
+	for (i = 0 ; i<= len - 1; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
