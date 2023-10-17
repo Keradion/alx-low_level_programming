2fcded286446e6f60673779b0ea9d16ece8cb3db
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-16 19:10:18 -0700

    removing spaces

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - copy one string into another string 
+ * _strcpy - copy contents of one pointer into othe
  * @src: string pointer variable
  * @dest: string pointer variable
  *
@@ -13,13 +13,13 @@ char *_strcpy(char *dest, char *src)
 	int i = 0;
 	int len = 0;
 
-	while ( src[i] != '\0')
+    while (src[i] != '\0')
 	{
 		len++;
 		i++;
 	}
 	
-	for (i = 0 ; i<= len ; i++)
+	for (i = 0 ; i <= len ; i++)
 	{
 		dest[i] = src[i];
 	}
