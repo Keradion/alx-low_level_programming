16c6568a0a52a5ad11e59e0711635a6b646fb03e
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-16 17:50:12 -0700

    print charachter of string staring with index 0 , even indexs

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - print every charachter of a string starting with first charachter
+ * @str: string pointer variable
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int len = 0;
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	
+	for (i = 0; i<= len -1; i = i + 2)
+	{
+	_putchar(str[i]);
+	}
+_putchar('\n');	
+}
