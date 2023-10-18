9d0e929fc3eaec921d8893841b08b985cf509db4
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-18 09:27:09 -0700

    lowercase string into uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * string_toupper - changer all strings into upperCase
+ * @str: string to be converted to uppercase
+ *
+ * Return: string in uppercase
+ */
+
+char *string_toupper(char *str)
+{
+	int j = 0;
+
+
+	while (str[j] != '\0')
+	{
+		
+		if ('a' <= str[j] && str[j] <= 'z')
+
+		{
+			str [j]  = str[j] - 32;
+		}
+		j++;
+	}
+
+	return (str);
+}
+
+
+
