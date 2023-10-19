5bc6b96f0d0e2fda638072ad9a92a00130cb6f74
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-19 03:26:28 -0700

    concatenate n charachters of string 2 into string 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - conacatenates n charachters of string 2 into string 1
+ * @dest: string 1
+ * @src: string 2
+ * @n: number of charachter to be concatenated
+ *
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int count = 0;
+
+	while (dest[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	i = 0;
+	for (i = 0 ; i <= (n - 1); i++)
+	{
+	dest[count++] = src[i];
+	}
+
+	return (dest);
+}
