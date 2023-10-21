b0382d41481bf277237abfceba9369dae48ce5ac
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-21 10:11:17 -0700

    function that prints a text using write function into standard output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - function that prints text on standard output
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
