ec81f4248924b7b95d034d4bc8214306a9b6e01e
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-22 05:14:49 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,27 +8,35 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
+	int i, j = 0;
 	int count = 0;
 	int sign = 0;
-	int digit = 0;
+        int digit = 0;
 	int result = 0;
 
-	if (s[0] == '-')
+	while (s[j] != '\0')
+	{
 
-		sign = -1;
-	else
+	if (s[j] == '-' || s[j] == '+')
+
+        sign = -1;
+
+	else 
 		sign = 1;
 
-	while (s[i] != '\0')
-	{
-		count++;
-		i++;
+
+	j++;
+	count++;
+
 	}
+
+
 	i = 0;
+
 	for (i = 0; i < count; i++)
 	{
 		digit = s[i] - '0';
+
 		if (digit >= 0 && digit <= 9)
 			result = result * 10 + digit;
 	}
