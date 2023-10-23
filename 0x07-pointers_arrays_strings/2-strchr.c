90d0185067367744f0870be4349b9dbb3c3952b0
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-23 07:31:15 -0700

    search a given charachter in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - function that lochates a character in a string
+ * @s: pointer to string
+ * @c: target character to be search
+ *
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+
+
+	{	
+		while (s[count] != c)
+	{
+
+		count++;
+	}
+
+	return (&s[count]);
+}
+
+return (0);
+}
+	
