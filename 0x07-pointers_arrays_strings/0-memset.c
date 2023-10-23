677d7d322ab31bc0f6dd94ee69f9504749b075f3
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-23 06:14:07 -0700

    a function that fill n bytes of  a memory block pointe by a pointer with a given byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - function that fill n bytes of the memeory area pointed to 
+ * by s with the constant byte b
+ * @s: pointer that point memeory location to be fill
+ * @b: constant byte (char)
+ * @n: bytes of memory to be fill
+ *
+ * Return: a pointer to s 
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n ; i++)
+	{
+		s[i] = b;
+	}
+
+	s[i] = '\0';
+
+	return (s);
+}
