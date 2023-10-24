275b9699449a35b01c09d2e6a58889e9738cb54d
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-24 02:07:47 -0700

    return null

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strpbrk -searches a string for any of a set of bytes
@@ -34,7 +33,7 @@ char *_strpbrk(char *s, char *accept)
 			return (&s[i]);
 	}
 	}
-	return NULL;
+	return ('\0');
 }
 
 
