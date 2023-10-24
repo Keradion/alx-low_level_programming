a96c9fa34fdb60306357b01df621639310c3cba5
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-24 05:39:44 -0700

    return first occurence of the substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - find first occurence of the substring needle in haystack
+ * @haystack: string
+ * @needle: string
+ *
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int len = 0;
+	int len1 = 0;
+
+	while (haystack[len] != '\0')
+		len++;
+	while (needle[len1] != '\0')
+		len1++;
+	for (i = 0 ; i < len1; i++)
+	{
+		for (j = 0; j < len; j++)
+		{
+			if (needle[i] == haystack[j])
+				return (&needle[i]);
+		}
+	}
+	return ('\0');
+}
+	
