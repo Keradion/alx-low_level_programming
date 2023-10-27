4e50f9b431dd76f18473f7d13225d369716e93ae
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-27 16:56:54 -0700

    betty correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,6 @@ int main(int argc, char *argv[])
 	int num2 = 0;
 
 	if (argc < 3)
-		
 	{
 		printf("%s\n", "Error");
 		return (1);
@@ -22,11 +21,9 @@ int main(int argc, char *argv[])
 
 	else
 	{
-
 	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]); 
+	num2 = atoi(argv[2]);
 	printf("%d\n", (num1 * num2));
-
 	}
 	return (0);
 }
