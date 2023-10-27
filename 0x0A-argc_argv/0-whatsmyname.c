1bef71e82e5902c3adcfa9408daade587ce17749
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-27 15:45:11 -0700

    program taht prints its name and use -Wno-unsed-parameter to by pass unused parameter argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line 
+ * @argc: number of arguments passed
+ * @argv: array of strings
+ *
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
