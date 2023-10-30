4d52860753358fac534be4bf3a128057bef61bcc
Daniel Berihun <Danielshitaye10@gmail.com>
2023-10-30 01:09:07 -0700

    function that reserve  a space and fill it with given string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - return a pointer to a newlly allocated space
+ * @str: string to be store in reserved space
+ *
+ * Return: char *
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr = '\0';
+	int size = 0;
+	int i = 0;
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+
+	ptr = (char *)malloc(size + 1);
+
+	if (ptr == NULL || str == NULL)
+
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = str[i];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
