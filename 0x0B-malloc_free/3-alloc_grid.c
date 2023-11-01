5a578a95d450d36ae9d3419cd7479f662512f720
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-01 09:44:37 -0700

    create a 2d array and reserve space using malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that return a pointer to a 2d array
+ * @width: column number
+ * @height: row number
+ *
+ */
+int **alloc_grid(int width, int height)
+{
+	int i = 0;
+	int **ptr = NULL;
+
+	ptr = (int **)malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = (int *)malloc(sizeof(int) * width);
+	}
+	return (ptr);
+}
