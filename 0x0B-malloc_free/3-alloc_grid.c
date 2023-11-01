96312a915c28ab0f5980cc9f19a9d3e606ecb788
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-01 12:37:04 -0700

    free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,11 +23,6 @@ int **alloc_grid(int width, int height)
 
 	if (ptr == NULL)
 	{
-		for (i = 0; i < height; i++)
-		{
-			free(ptr[i]);
-		}
-		free(ptr);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
