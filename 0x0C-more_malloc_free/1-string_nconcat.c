35c4b0db22e800cdb20983909ebe04d746909885
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-02 15:41:08 -0700

    j

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,6 +22,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[len2] != '\0')
 		len2++;
 
+	if (s1 == NULL)
+		len1 = 0;
+	if (s2 == NULL)
+		len2 = 0;
+
 	if (n >= len2)
 		n = len2;
 
