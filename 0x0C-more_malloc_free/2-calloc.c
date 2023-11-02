0f7751c71606d47929f867663af45979caa05381
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-02 07:09:39 -0700

    set memory to zero

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,6 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	 int *ptr = NULL;
+	 unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,5 +21,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = 0;
+	}
+	ptr[i] = '\0';
+
 	return (ptr);
 }
