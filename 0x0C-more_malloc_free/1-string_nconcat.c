ecc94af0b2f5dee656e583a6b8ea8d3977e049a2
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-02 15:50:05 -0700

    h

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
-	ptr = malloc(len1 + len2 + 1);
+	ptr = malloc(len1 + n + 1);
 
 	if (ptr == NULL)
 		return (NULL);
