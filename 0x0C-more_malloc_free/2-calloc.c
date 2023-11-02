9c650729bcbca21af2e6cc69f7b5228ad52967b3
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-02 07:17:48 -0700

    fill each bytes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	 int *ptr = NULL;
+	 char *ptr = NULL;
 	 unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb * size; i++)
+	for (i = 0; i < nmemb; i++)
 	{
 		ptr[i] = 0;
 	}
