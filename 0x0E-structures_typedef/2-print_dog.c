f4a7a9cc6bda0042629f96dc82e609f19d3df361
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-07 00:22:20 -0800

    removal of functions to fulfill the requirnments

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,5 @@
-#include "dog.h"
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * print_dog - prints struct dog
@@ -15,14 +15,9 @@ void print_dog(struct dog *d)
 	/*control will be back to caller if ptr has null value */
 	if (d->name == NULL)
 		printf("Name: (nill)\n");
-	else
-		 printf("Name: %s\n", (d->name));
 	if (d->age == '\0')
 		printf("Age: (nill)\n");
-	else
-		printf("Age: %f\n", (d->age));
 	if (d->owner == NULL)
-		printf("Owner: (nill)\n");
-	else
-		printf("Owner: %s\n", (d->owner));
+		printf("Owner: (nill)");
+	printf("Name: %s\nAge: %f\nOwner %s\n", d->name, d->age, d->owner);
 }
