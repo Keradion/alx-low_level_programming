69a439944070132a7becff4b1848ed4adfb257ec
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-07 00:23:32 -0800

    removal of functions to fulfill the requirnments

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,5 +19,5 @@ void print_dog(struct dog *d)
 		printf("Age: (nill)\n");
 	if (d->owner == NULL)
 		printf("Owner: (nill)");
-	printf("Name: %s\nAge: %f\nOwner %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
