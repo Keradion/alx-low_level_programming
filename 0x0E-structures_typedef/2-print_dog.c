e455fdbcc2a377881c6f260fda392b483b7a75ae
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-07 00:42:31 -0800

    betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,11 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	/*control will be back to caller if ptr has null value */
+
 	if ((*d).name == NULL)
 		(*d).name = "(nill)";
 	if ((*d).owner == NULL)
 		(*d).owner = "(nill)";
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
