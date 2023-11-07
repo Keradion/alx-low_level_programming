9f3a118902c3bae35ce87afb51e99aa4bcfbdca3
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-07 00:26:35 -0800

    description added fot struct dog inside dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure type for a dog
+ * @name: member to hold name
+ * @age: member to hold age
+ * @owner: member to hold owner
+ *
+ * Description: struct dog structure data type
+ */
+ 
 struct dog 
 {
 	char *name;
