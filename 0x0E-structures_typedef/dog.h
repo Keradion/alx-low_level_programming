cef05c55c3d4c2011856e275611b0822fdfebefa
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-06 22:02:51 -0800

    struct dog data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * struct dog - create a structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * a structure that define struct dog data type with name, age & owner members
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
