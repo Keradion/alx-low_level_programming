3279538bed0623a2d8f0cd8cbc1d93d0b6e733fb
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-09 11:37:24 -0800

    separater

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 
-		if (separator != NULL)
+		if (i < (n-1))
 		printf("%c ", *separator);
 
 		i++;
