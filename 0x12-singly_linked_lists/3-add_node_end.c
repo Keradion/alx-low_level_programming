285e57136099e0103971becbba8042ffa77efa13
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-16 13:26:53 -0800

    main.c files added

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add a new node at the end of a list
+ * @str: data inside the node
+ * @head: address of the head node
+ *
+ * Return: address of the new node , null if failure occure
+ */
+
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
