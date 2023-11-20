01150a4afa2ab49e7a70cc94eea8c94de297a112
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-20 12:01:44 -0800

    temp

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,26 +14,26 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new;
 	listint_t *temp = *head;
 
+
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
 		return (NULL);
 
+	  new->next = NULL;
+	  new->n = n;
+
 	if (*head == NULL)
 	{
-		new->n = n;
-		new->next = NULL;
 		*head = new;
 		return (*head);
 	}
 
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-			new->n = n;
-			new->next = NULL;
-		}
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
 		temp->next = new;
 
-	return (temp);
+	return (new);
 }
