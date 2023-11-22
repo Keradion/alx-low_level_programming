abe25b3f9c7d9876edd38a1f345d60f0e273f7e7
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-22 15:15:17 -0800

    if no list exist

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,6 +14,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count = 0;
 	listint_t *temp = head;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (head->next != NULL)
 	{
 		head = head->next;
