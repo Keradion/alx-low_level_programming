e3674f3261e3bbe34e5b03aa1aa5e22f4ca919de
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-22 13:15:04 -0800

    frees a space used by a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t linked list
+ * @head: pointer to the head node
+ *
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+
+	while (head->next != NULL)
+	{
+		temp = head;
+		head = head->next;
+		temp->next = NULL;
+		free(temp);
+	}
+	free(head);
+}
