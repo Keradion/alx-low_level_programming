0a4e5509327e1fcf2a57dc764c9e15c928183737
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-22 14:32:24 -0800

    fun that remove head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head noad of listint_t
+ * @head: double pointer to the head node
+ *
+ * Return: 0 if no node exists
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	if (head == NULL)
+		return (0);
+
+	*head = temp->next;
+	temp->next = NULL;
+	free(temp);
+
+	return (1);
+}
