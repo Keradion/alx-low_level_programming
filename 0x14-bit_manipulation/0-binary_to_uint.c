b833cc381c4f6dc42cdcbf528537b98691eae335
Daniel Berihun <danielshitaye10@gmail.com>
2024-02-09 02:59:29 -0800

    binary to deciaml

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: pointer to string of bits
+ *
+ * Return: 0 or deciaml
+ */
+
+ 
+
+unsigned int binary_to_uint(const char *b)
+{
+	const char *temp = b;
+	int bits = 0;
+	unsigned decimal = 0;
+	int i = 1;
+
+	if (b == NULL) 
+		return 0;
+
+	while(*temp != '\0')
+	{
+		bits++; /* counting number of bits */
+		temp++;
+	}
+
+
+	for(i = 1; i <= bits; i++)
+	{
+		if (*b != '1' && *b != '0')
+			return 0;
+
+		decimal = decimal + ( (*b++) * power(bits - i));
+
+	}
+	return decimal;
+}
+
+int power(int n)
+{
+	int result = 1;
+	int exponent = n;
+	int i = 1;
+
+	if (exponent == 0)
+		return 1;
+
+
+	while(i <= exponent)
+	{
+		result = result * 2;
+		i++;
+	}
+
+	return result;
+}
+	
