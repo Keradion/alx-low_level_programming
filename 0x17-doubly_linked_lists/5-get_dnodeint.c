a8c6259702cd80c710111dc7e199313f9a2706bd
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-22 07:35:32 -0700

    get node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,8 +12,21 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *temp = head;
 	size_t i = 0;
+	size_t node_number = 0;
 
 	if (head == NULL)
+		return (NULL);
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		node_number++;
+	}
+
+	temp = head;
+
+
+	if (index >= node_number)
 		return (NULL);
 
 	while (i < (index))
