7437f5dfbf0fb2a47c0ddb47118102d47c131ce2
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-24 03:58:42 -0700

    create a file and set permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,11 +21,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		write(fd, text_content, text_size);
+		close(fd);
+		return (1);
 	}
+	
+	write(fd, text_content, text_size);
 
 	close(fd);
 
